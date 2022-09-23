0073041823a0bf299df4cb03492d7468131f2b6d
Raúl Castro Estévez <raulcastroestevez@helindata.com>
2022-09-23 17:27:52 +0200

    Conftest

--- orm.py
+++ orm.py
@@ -1,5 +1,5 @@
 import model
-from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import mapper, relationship
 
 
@@ -20,9 +20,18 @@ batches = Table(
 
 allocations = Table(
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("order_id", Integer),
-    Column("batch_id", Integer),
+    Column("order_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
 )
 
 def start_mappers():
-    lines = mapper(model.OrderLine, order_lines)
+    lines_mapper = mapper(model.OrderLine, order_lines)
+    mapper(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper, secondary=allocations, collection_class=set,
+            )
+        },
+    )
