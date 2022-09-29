3228d8e40abc743146d1ef23a1ab5119e0657155
Raúl Castro Estévez <raulcastroestevez@helindata.com>
2022-09-29 12:17:25 +0200

    Some adaptions

--- tests/e2e/redis_client.py
+++ tests/e2e/redis_client.py
@@ -1,5 +1,15 @@
 import redis
 from allocation import config
+from allocation.domain import events
+
 
 r = redis.Redis(**config.get_redis_host_and_port())
 
+def subscribe_to(channel: str):
+    pubsub = r.pubsub(ignore_subscribe_messages=True)
+    pubsub.subscribe("change_batch_quantity")
+
+
+def publish_to(channel: str, event: events.Event):
+    pubsub = r.pubsub(ignore_subscribe_messages=True)
+    pubsub.publish("change_batch_quantity") 
